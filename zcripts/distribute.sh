eaa716717f3cc61b680d141436ffc658e298306b
Paul Albrecht Gronau <paul.gronau@uni-oldenburg.de>
2020-01-10 12:08:31 +0100

    Distributing models script added.

---
+++ zcripts/distribute.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for server in "accum" "bagband" "cleverns" "driefel" "etzel" "horsten" "ihlow" "jeringhave" "kranenkamp" "langendamm" "mariensiel" "neuenburg" # "funnix" "grabstede"
+
+do
+  echo $server
+  scp ./docker_pw.txt $server:/home/infocup/docker_pw.txt
+  ssh 'docker cat ~/docker_pw.txt | docker login --username pgronau --password-stdin; \
+  docker run -d pgronau/infocup:slave;\
+  docker run -d pgronau/infocup:slave;\
+  docker run -d pgronau/infocup:slave;\
+  docker run -d pgronau/infocup:slave;\'
+done
